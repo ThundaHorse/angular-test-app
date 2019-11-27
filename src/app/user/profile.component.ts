1e1ad3e737261a0433998fdba366adafa5670800
ThundaHorse <abeeeeekim@gmail.com>
2019-11-26 23:47:52 -0500

    initial implementation of reactive/model form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,12 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: "",
-  template: `
-    <div class="container">
-      <h1>Test</h1>
-    </div>
-  `,
+  templateUrl: "./profile.component.html",
   styles: [``]
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    let firstName = new FormControl(this.auth.abe.firstName);
+    let lastName = new FormControl(this.auth.abe.lastName);
+    let userName = new FormControl(this.auth.abe.userName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName,
+      userName: userName
+    });
+  }
+
+  cancel() {
+    let firstName = new FormControl("");
+    let lastName = new FormControl("");
+    let userName = new FormControl("");
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName,
+      userName: userName
+    });
+  }
+}
